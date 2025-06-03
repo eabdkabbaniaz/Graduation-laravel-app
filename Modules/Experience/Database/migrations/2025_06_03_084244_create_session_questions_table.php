79aefe097a8210ff8eb2176742ab7a7fdc78a87b
eabd kab <eabd@gmail.com>
2025-06-03 21:17:06 +0300

    report & experience question

--- Modules/Experience/Database/migrations/2025_06_03_084244_create_session_questions_table.php
+++ Modules/Experience/Database/migrations/2025_06_03_084244_create_session_questions_table.php
@@ -11,12 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('session_users', function (Blueprint $table) {
+        Schema::create('session_questions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId( 'session_id')->constrained('sessions')->onDelete('cascade');
-            $table->foreignId( 'user_id')->constrained('users')->onDelete('cascade');
-            $table->double('mark')->default(0.0);
-              $table->string('report')->nullable();
+            $table->string('question');
+            $table->enum('question_mode', ['fixed', 'dynamic']);
+            $table->foreignId('session_id')->constrained('sessions')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -26,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('session_users');
+        Schema::dropIfExists('session_questions');
     }
 };

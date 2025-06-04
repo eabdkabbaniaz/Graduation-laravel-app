e10d5bba03f182109c9bd312b9bb65275087d1be
eabd kab <eabd@gmail.com>
2025-06-04 14:47:46 +0300

    upload file

--- Modules/Traits/UploadServiceTrait.php
+++ Modules/Traits/UploadServiceTrait.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Modules\Traits;
 
 use Illuminate\Http\UploadedFile;
@@ -6,11 +7,24 @@ use Illuminate\Support\Facades\Storage;
 
 class UploadServiceTrait
 {
-   
-    
-    public static function uploadFile(UploadedFile $file, string $folder): string
+
+
+   /* public static function uploadFile(UploadedFile $file, string $folder): string
     {
+
         $filename = time() . '_' . uniqid() . '.' . $file->getClientOriginalExtension();
-        return $file->storeAs($folder, $filename, 'public'); // storage/app/public/{folder}/filename
-    }
+        $destinationPath = public_path($folder); 
+        if (!file_exists($destinationPath)) {
+            mkdir($destinationPath, 0755, true);
+        }
+        $file->move($destinationPath, $filename);
+        return asset($folder . '/' . $filename);
+    }*/
+    public static function uploadFile(UploadedFile $file, string $folder): string
+{
+    $filename = time() . '_' . uniqid() . '.' . $file->getClientOriginalExtension();
+    $path = $file->storeAs($folder, $filename, 'public'); 
+    return asset('storage/' . $path);
+}
+
 }
